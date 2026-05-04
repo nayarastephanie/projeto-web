84dd3ba535845374f575dc15286780affed1d7a4
Nayara Silva <nayara.silva@compliancesolucoes.com.br>
2026-05-04 11:23:19 -0300

    mudanças estilizaçao

--- main.py
+++ main.py
@@ -5,30 +5,28 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# arquivos estáticos (CSS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-# "Banco de dados" em memória
 usuarios = []
 
-# Página inicial (formulário + conteúdo)
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
 
-# Recebe dados do formulário
 @app.post("/adicionar")
 def adicionar(nome: str = Form(...)):
     usuarios.append(nome)
     return RedirectResponse(url="/usuarios", status_code=303)
 
-# Lista usuários
 @app.get("/usuarios", response_class=HTMLResponse)
 def listar(request: Request):
-    return templates.TemplateResponse("usuarios.html", {
-        "request": request,
-        "usuarios": usuarios
-    })
+    return templates.TemplateResponse(
+        "usuarios.html",
+        {"request": request, "usuarios": usuarios}
+    )
